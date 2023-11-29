51287458fbd17fd676f8e980680c94be6a8faf08
sumit23802380 <sumitjangid1692153@gmail.com>
2023-11-29 21:42:55 +0530

    Added : UC3 Implemented Enqueue Method for Queue

--- src/com/bridgelabz/stackandqueue/Main.java
+++ src/com/bridgelabz/stackandqueue/Main.java
@@ -1,13 +1,10 @@
 package com.bridgelabz.stackandqueue;
 public class Main {
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(56);
-        stack.push(30);
-        stack.push(70);
-//        stack.display();
-        System.out.println(stack.peek());
-        System.out.println(stack.pop());
-        stack.display();
+        Queue<Integer> queue = new Queue<>();
+        queue.enqueue(56);
+        queue.enqueue(30);
+        queue.enqueue(70);
+        queue.display();
     }
 }
--- src/com/bridgelabz/stackandqueue/Queue.java
+++ src/com/bridgelabz/stackandqueue/Queue.java
@@ -1,4 +1,28 @@
 package com.bridgelabz.stackandqueue;
 
-public class Queue {
+public class Queue <T extends Comparable<T>>{
+    private final LinkedList<T> linkedList;
+
+    /*
+     * @desc : Constructor to Initialize queue this also initialize linked-list
+     */
+    public Queue() {
+        this.linkedList = new LinkedList<>();
+    }
+    /*
+     * @desc : adds the data to the head of the linked-list or end of the queue
+     * @params : Generic data
+     * @return :
+     */
+    public void enqueue(T data){
+        linkedList.append(data);
+    }
+    /*
+     * @desc : prints the queue
+     * @params :
+     * @return :
+     */
+    public void display(){
+        linkedList.display();
+    }
 }
